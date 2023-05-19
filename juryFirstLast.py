1bebfc02b1da3a33a3ea666b010cbbf31897b7f9
JeremyBallard <46586431+JeremyBallard@users.noreply.github.com>
2023-05-18 21:26:56 -0700

    Switch to Selenium

--- juryFirstLast.py
+++ juryFirstLast.py
@@ -1,10 +1,11 @@
-from requests import Session, Request
+from selenium import webdriver
+import pandas as pd
 
-euroSession = Session()
-#bypass cloudflare anti bot
-#https://stackoverflow.com/questions/70049808/python-requests-how-to-bypass-checking-your-browser-for-5-second-thing
-headers = {'User-Agent': 'Mozilla/5.0'}
-
-page = euroSession.get('https://eurovision.tv/event/liverpool-2023/grand-final/results/albania', headers=headers)
-print(page.text)
-
+driver = webdriver.Firefox()
+#eventually make a whole ass loop where the ending is the country name appended to the url
+driver.get('https://eurovision.tv/event/liverpool-2023/grand-final/results/albania')
+#absolute XPATH because I have no idea how to do relative yet
+table = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[3]/div/div/main/section/div/div[2]/div/div/div[2]/div/div/div[2]/div/table')
+#find first(?) instance of rank, which would be jury vote rank
+print(table.find_element(By.CLASS_NAME, 'Rank'))
+driver.quit()
